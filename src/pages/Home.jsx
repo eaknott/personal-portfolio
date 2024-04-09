5618aa29e942605e686db86972e2ea61c4b48bbf
eaknott <emknotty@gmail.com>
2024-04-09 18:39:43 +0800

    Add media queries for iPad screen for Home page, About page, Contact page, Header component, and Footer component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <p>I'm a frontend developer, animal lover, environmental advocate, and passionate learner. Welcome to my website. To learn more about me, click around!</p>
       </div>
       <div className="home-img">
-        <img src={ProfileImage} alt="profile" height="250rem" />
+        <img src={ProfileImage} alt="profile" />
       </div>
     </div>
   )
