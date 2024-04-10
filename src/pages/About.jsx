5e8e79be0af203a7e028170c706a6919ce8c7386
eaknott <emknotty@gmail.com>
2024-04-10 14:38:59 +0800

    Change/Add accessibility sections for About, Contact, and Skills pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,12 @@ function About() {
   return (
     <div className="about">
         <div className="about-text">
-            <h1>More About Me</h1>
-            <p>Born and raised in the Washington DC area, I'm now based in Taipei. After finishing my Bachelor's degree in Foreign Languages with a Business minor, I moved to Taiwan to be closer to my family and get in touch with my Taiwanese roots. I've enjoyed exploring various roles such as teaching kindergarten ESL, dancing professionally for special events, and working in audio editing for e-learning platforms.</p>
+            <header>
+              <h1>More About Me</h1>
+            </header>
+            <main>
+              <p>Born and raised in the Washington DC area, I'm now based in Taipei. After finishing my Bachelor's degree in Foreign Languages with a Business minor, I moved to Taiwan to be closer to my family and get in touch with my Taiwanese roots. I've enjoyed exploring various roles such as teaching kindergarten ESL, dancing professionally for special events, and working in audio editing for e-learning platforms.</p>
+            </main>
         </div>
     </div>
   )
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,13 +1,13 @@
 function Skills() {
   return (
     <div className="skills">
-      <div className="skills-text">
+      <header className="skills-text">
         <h1>Technical Skills</h1>
         <p>Here are a few of the technologies that I've worked with recently. Feel free to <a href="https://www.linkedin.com/in/emilyaknott/" target="_blank" rel="noreferrer">check out my LinkedIn profile</a> for more details.</p>
-      </div>
-      <div className="skills-body">
-        
-      </div>
+      </header>
+      <main className="skills-body">
+
+      </main>
     </div>
   )
 }
