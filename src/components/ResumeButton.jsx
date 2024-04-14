9b65d00b9fb6441b1332386a88b4c936892087c7
eaknott <emknotty@gmail.com>
2024-04-14 16:28:50 +0800

    Add file name to save as in download attribute

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -3,7 +3,7 @@ import myCV from '../assets/EMILY KNOTT CV.jpg'
 function ResumeButton() {
   return (
     <div>
-        <a href={myCV} className="resume-download" download>
+        <a href={myCV} className="resume-download" download="EMILY_ KNOTT_CV.jpg">
             <button>Download My Resume</button>
         </a>
     </div>
