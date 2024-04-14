2fe531cb1ba9efd0b62a604b8a1498654600c22f
eaknott <emknotty@gmail.com>
2024-04-14 16:29:18 +0800

    Add ResumeButton component to Skills and Contact pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <div className="contact">
         <section className="contact-text">
             <h1>Get in Touch</h1>
-            <p>Contact me if you would like to say hello or learn more about my experience. Send me an email or a message over LinkedIn, or leave a message below. I will get back to you as soon as I can.</p>
+            <p>Contact me if you would like to say hello or learn more about my experience. You can send me an email or a message over LinkedIn, or leave a message down below. I will get back to you as soon as I can.</p>
         </section>
         <section className="contact-info">
           <p>Reach me by email: <span className="contact-email">emilyknott.dev@gmail.com</span></p>
