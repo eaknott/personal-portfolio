39f0b35604956841226b8a7b80865378df18ce0f
Emily Knott <emknotty@gmail.com>
2024-07-27 11:50:50 +0800

    Edit home page intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
       <div className="home-text">
         <div className="intro">Hello, my name is </div>
         <h1>Emily Knott.</h1>
-        <p>I'm a full-stack developer, specializing in <span>React</span> web applications. Currently, I'm focused on building applications with <span>Python</span> and the <span>Django</span> framework, using <span>PostgreSQL</span>.</p>
+        <p>I'm a software engineer, specializing in <span>React</span> web applications. Currently, I'm focused on building applications with <span>Python</span> and the <span>Django</span> framework, using <span>PostgreSQL</span>.</p>
         <a href="#projects"><button>Check out my portfolio!</button></a>
       </div>
     </div>
